a106ffbc2cf588673e4ec57f40ea1e54a5329336
Guillaume Meyer <guillaume.meyer@outlook.com>
2022-02-06 15:14:37 +0100

    initial commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,188 @@
+module.exports = {
+  base: '/',
+  title: 'Docs',
+  description: 'WhatStack documentation',
+  head: [],
+  dest: 'dist',
+  plugins: [
+    '@vuepress/active-header-links', {
+      sidebarLinkSelector: '.sidebar-link',
+      headerAnchorSelector: '.header-anchor',
+      headerTopOffset: 120
+    },
+    '@vuepress/last-updated', {
+      transformer: (timestamp, lang) => {
+        const moment = require('moment')
+        moment.locale(lang)
+        return moment(timestamp).fromNow()
+      }
+    },
+    ['mermaidjs',
+      {
+        theme: 'neutral' // default, dark, forest, neutral
+      }
+    ],
+    [
+      "@mr-hope/copy-code",
+      {
+        // your options
+      },
+    ],
+    'autometa', {
+      enable: true, // enables/disables everything - control per page using frontmatter
+      image: true, // regular meta image used by search engines
+      twitter: true, // twitter card
+      og: true, // open graph: facebook, pinterest, google+
+      schema: true,
+      canonical_base: 'https://docs.whatstack.io',
+      author: {
+        name: 'Guillaume Meyer',
+        twitter: 'guillaumemeyer',
+      },
+      site: {
+        name: 'WhatStack Docs',
+        twitter: '',
+      },
+      description_sources: [
+        'frontmatter',
+        'excerpt',
+        // markdown paragraph regex
+        //
+        /^((?:(?!^#)(?!^\-|\+)(?!^[0-9]+\.)(?!^!\[.*?\]\((.*?)\))(?!^\[\[.*?\]\])(?!^\{\{.*?\}\})[^\n]|\n(?! *\n))+)(?:\n *)+\n/img,
+        //
+        // this excludes blockquotes using `(?!^>)`
+        ///^((?:(?!^#)(?!^\-|\+)(?!^[0-9]+\.)(?!^!\[.*?\]\((.*?)\))(?!^>)(?!^\[\[.*?\]\])(?!^\{\{.*?\}\})[^\n]|\n(?! *\n))+)(?:\n *)+\n/img,
+
+        // html paragraph regex
+        /<p(?:.*?)>(.*?)<\/p>/i,
+
+      ],
+      // ---------------------------------------------------------------------------
+      // order of what gets the highest priority:
+      // 1. frontmatter
+      // 2. content markdown image such as `![alt text](http://url)`
+      // 3. content regular html img
+      image_sources: [
+        'frontmatter',
+        /!\[.*?\]\((.*?)\)/i,        // markdown image regex
+        /<img.*?src=['"](.*?)['"]/i, // html image regex
+      ]
+    }
+  ],
+
+  theme: 'yuu',
+
+  themeConfig: {
+
+    yuu: {
+      defaultDarkTheme: true,
+      disableDarkTheme: false,
+      colorThemes: ['blue', 'purple'], // green (default), blue, red, and purple
+      defaultColorTheme: 'blue',
+      labels: {
+        darkTheme: '🌗 Dark Theme', // Default is "Enable Dark Theme?"
+        ignoreThemes: 'Ignore themes?', // Default is "Ignore Other Themes?"
+      },
+      disableThemeIgnore: true
+    },
+
+    logo: '',
+
+    // Footer options
+    lastUpdated: '♻️ Last Updated',
+
+    // Smooth scrolling
+    smoothScroll: true,
+
+    // GitHub options
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'whatstack',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: '🐙 GitHub',
+    // Optional options for generating "Edit this page" link
+    //   if your docs are in a different repo from your main project:
+    docsRepo: 'whatstack/docs',
+    //   if your docs are not at the root of the repo:
+    docsDir: 'src',
+    //   if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'main',
+    //   defaults to false, set to true to enable
+    editLinks: true,
+    //  custom text for edit link. Defaults to "Edit this page"
+    editLinkText: '🌈 Improve this page!',
+
+    // Navbar: Horizontal navigation
+    nav: [
+      { text: 'App', link: 'https://app.whatstack.ioo' }
+      // {
+      //   text: '🚀 Developers',
+      //   ariaLabel: 'Developers',
+      //   items: [
+      //     { text: 'API Explorer', link: '/api/explorer' }
+      //   ]
+      // }
+    ],
+
+    // Sidebar options
+    displayAllHeaders: false,
+    activeHeaderLinks: true,
+    sidebarDepth: 1,
+
+    // Sidebar: Vertical navigation
+    sidebar: {
+      '/': [
+        {
+          title: 'WhatStack Documentation',
+          path: '/'
+        },
+        getSideBar('/how-to', 'How to?')
+        // {
+        //   title: 'No-Code Solutions',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['/nocode/', 'Get Started'],
+        //     ['/nocode/naming-conventions.md', 'Naming conventions'],
+        //     ['/nocode/approval.md', 'Approval workflow'],
+        //     ['/nocode/audience-targeting.md', 'Audience Targeting'],
+        //     ['/nocode/ad-schema-extensions.md', 'AD Schema Extensions']
+        //   ]
+        // },
+      ]
+    }
+  }
+}
+
+
+function getSideBar (folder, title) {
+  const fs = require('fs')
+  const path = require('path')
+  const titleCasing = require('title-case')
+  const sentenceCasing = require('sentence-case')
+  const extension = ['.md']
+  const childrens = []
+
+  const files = fs
+    .readdirSync(path.join(`${__dirname}/../${folder}`))
+    .filter(
+      (item) =>
+        item.toLowerCase() !== 'readme.md' &&
+        fs.statSync(path.join(`${__dirname}/../${folder}`, item)).isFile() &&
+        extension.includes(path.extname(item))
+    )
+    .forEach((file, fileIndex) => {
+      let fileTitle = path.parse(file).name
+      fileTitle = fileTitle.replace('/-/g', ' ')
+      fileTitle = fileTitle.replace('/_/g', ' ')
+      fileTitle = sentenceCasing.sentenceCase(fileTitle)
+      childrens.push([folder + '/' + path.parse(file).name, fileTitle])
+    })
+
+  return {
+    title: titleCasing.titleCase(title),
+    collapsable: true,
+    sidebarDepth: 1,
+    children: childrens
+  }
+}
